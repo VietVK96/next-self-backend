1e645896b353aa411c49ee7cc239857c0a9240e3
phuong <otcute@gmail.com>
2023-06-02 17:28:19 +0700

    convert entities storage-space-pack.entity.ts

---
+++ src/entities/storage-space-pack.entity.ts
@@ -0,0 +1,68 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+/**
+ * @ORM\Entity(repositoryClass="\App\Repositories\Storage\Space\Pack")
+ * @ORM\Table(name="T_STORAGE_SPACE_PACK_STK")
+ */
+@Entity('T_STORAGE_SPACE_PACK_STK')
+export class StorageSpacePack {
+  /**
+   * Identifiant de l'enregistrement.
+   *
+   * @ORM\Column(name="STK_ID", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="IDENTITY")
+   * @Expose
+   * @var integer
+   */
+  @PrimaryGeneratedColumn('increment', {
+    name: 'STK_ID',
+  })
+  id?: number;
+
+  /**
+   * @ORM\Column(name="STK_SIZE", type="bigint")
+   * @var integer Taille en octet du pack d'espace de stockage.
+   */
+  @Column({
+    name: 'STK_SIZE',
+    type: 'bigint',
+    length: 20,
+  })
+  size?: number;
+
+  /**
+   * @ORM\Column(name="STK_SIZE_READABLE", type="string", length=45)
+   * @var string Taille du pack d'espace de stockage au format
+   * chaîne de caractères.
+   */
+  @Column({
+    name: 'STK_SIZE_READABLE',
+    type: 'varchar',
+    length: 45,
+  })
+  sizeReadable?: string;
+
+  /**
+   * Prix du pack.
+   *
+   * @ORM\Column(name="STK_PRICE", type="decimal", precision=10, scale=2)
+   * @Expose
+   * @var float
+   */
+  @Column({
+    name: 'STK_PRICE',
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+  })
+  price?: number;
+
+  /** File: application\Entities\Storage\Space\Pack.php
+   * @ORM\OneToMany(targetEntity="\App\Entities\Storage\Space", mappedBy="storageSpacePack")
+   * @var \App\Entities\Storage\Space Espace de stockage par groupe.
+   */
+  // @TODO EntityMissing
+  //   protected $storageSpace;
+}
+// application/Entities/Storage/Space/Pack.php
