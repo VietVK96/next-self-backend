85419abfdce1397dd702455fc067232a690f5a54
congcuong095 <congcuong095@gmail.com>
2023-06-02 14:55:21 +0700

    - medical-header.entity.ts - medical-order.entity.ts - paypal-payment.entity.ts - phone-type.entity.ts - plan-event.entity.ts

---
+++ src/entities/plan-event.entity.ts
@@ -0,0 +1,63 @@
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="T_PLAN_EVENT_PLV")
+ */
+@Entity('T_PLAN_EVENT_PLV')
+export class PlanEventEnity {
+
+  /**
+   * @ORM\Column(name="PLV_POS", type="integer", nullable=false)
+   */
+  @Column({
+    name: 'PLV_POS',
+    type: 'integer',
+    nullable: false,
+    default: 1
+  })
+  pos?: number;
+
+  /**
+   * @ORM\Column(name="PLV_DELAY", type="integer", nullable=false)
+   */
+  @Column({
+    name: 'PLV_DELAY',
+    type: 'integer',
+    nullable: false,
+    default: 7
+  })
+  delay?: number;
+
+  /**
+   * Durée de la séance.
+   * 
+   * @ORM\Column(name="duration", type="time", nullable=true)
+   * @var \DateTime|null
+   */
+  @Column({
+    name: 'duration',
+    type: 'time',
+    nullable: true
+  })
+  duration?: string;
+
+  /**
+   * @ORM\ManyToOne(targetEntity="\App\Entities\Plan", inversedBy="events")
+   * @ORM\JoinColumn(name="PLF_ID", referencedColumnName="PLF_ID")
+   */
+  // @TODO EntityMissing
+  // protected $plan;
+
+  /**
+   * @ORM\OneToOne(targetEntity="\App\Entities\Event", inversedBy="planEvent")
+   * @ORM\JoinColumn(name="EVT_ID", referencedColumnName="EVT_ID")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="NONE")
+   */
+  // @TODO EntityMissing
+  // protected $event;
+
+}
+
+// application/Plan/Event.php
