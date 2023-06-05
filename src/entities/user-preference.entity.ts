4d8f2f611db2a0d1c3b694dd8e00e5de79d0a01c
congcuong095 <congcuong095@gmail.com>
2023-06-05 10:25:47 +0700

    - user-sms.entity.ts - user-type.entity.ts - wzagenda-contact.entity.ts

--- src/entities/user-preference.entity.ts
+++ src/entities/user-preference.entity.ts
@@ -6,6 +6,20 @@ export enum UserPreferenceViewType {
   WEEK = 'week'
 }
 
+// enum('none', 'both', 'three')
+export enum UserPreferenceQuotationDisplayOdontogramType {
+  NONE = 'none',
+  BOTH = 'both',
+  THREE = 'three'
+}
+
+//enum('none', 'both', 'only')
+export enum UserPreferenceQuotationDisplayDetailsType {
+  NONE = 'none',
+  BOTH = 'both',
+  ONLY = 'only'
+}
+
 /**
  * @ORM\Entity
  * @ORM\Table(name="T_USER_PREFERENCE_USP")
@@ -36,6 +50,22 @@ export class UserPreferenceEntity {
     default: 'FR'
   })
   country?: string;
+
+  /**
+   * Code pays.
+   * 
+   * @ORM\Column(name="USP_COUNTRY", type="string", length=3)
+   * @Expose
+   * @var string
+   */
+  @Column({
+    name: 'USP_COUNTRY',
+    type: 'varchar',
+    length: 3,
+    nullable: false,
+    default: 'FR'
+  })
+  countryCode?: string;
 
   /**
    * @ORM\Column(name="USP_TIMEZONE", type="string", length=45, nullable=false)
@@ -99,43 +129,55 @@ export class UserPreferenceEntity {
     length: 4,
     default: 1
   })
-  weekStartDay;
+  weekStartDay?: number;
 
   /**
    * @ORM\Column(name="USP_DISPLAY_HOLIDAY", type="integer", nullable=false)
    */
   @Column({
-    name: "USP_DISPLAY_HOLIDAY",
-    type="integer", nullable=false)
+    name: 'USP_DISPLAY_HOLIDAY',
+    type: 'tinyint',
+    nullable: false,
+    length: 1,
+    default: 0
   })
-  displayHoliday;
+  displayHoliday?: number;
 
   /**
    * @ORM\Column(name="USP_DISPLAY_EVENT_TIME", type="integer")
    */
   @Column({
-    name: "USP_DISPLAY_EVENT_TIME",
-    type="integer")
+    name: 'USP_DISPLAY_EVENT_TIME',
+    type: 'tinyint',
+    length: 1,
+    nullable: false,
+    default: 0
   })
-  displayEventTime;
+  displayEventTime?: number;
 
   /**
    * @ORM\Column(name="USP_DISPLAY_LAST_PATIENTS", type="integer")
    */
   @Column({
-    name: "USP_DISPLAY_LAST_PATIENTS",
-    type="integer")
+    name: 'USP_DISPLAY_LAST_PATIENTS',
+    type: 'tinyint',
+    length: 1,
+    nullable: false,
+    default: 1
   })
-  displayLastPatients;
+  displayLastPatients?: number;
 
   /**
    * @ORM\Column(name="USP_DISPLAY_PRACTITIONER_CALENDAR", type="integer")
    */
   @Column({
-    name: "USP_DISPLAY_PRACTITIONER_CALENDAR",
-    type="integer")
+    name: 'USP_DISPLAY_PRACTITIONER_CALENDAR',
+    type: 'tinyint',
+    length: 1,
+    nullable: false,
+    default: 0
   })
-  displayPractitionerCalendar;
+  displayPractitionerCalendar?: number;
 
   /**
    * @ORM\Column(name="USP_ENABLE_EVENT_PRACTITIONER_CHANGE", type="integer")
@@ -143,240 +185,358 @@ export class UserPreferenceEntity {
    * d'un patient dans la saisie d'un rendez-vous
    */
   @Column({
-    @ORM\Column(name="USP_ENABLE_EVENT_PRACTITIONER_CHANGE", type="integer")
-le: ventP
-actitionerChange;
+    name: 'USP_ENABLE_EVENT_PRACTITIONER_CHANGE',
+    type: 'tinyint',
+    length: 1,
+    nullable: false,
+    default: 1
+  })
+  enableEventPractitionerChange?: number;
 
   /**
    * @ORM\Column(name="USP_FREQUENCY", type="integer", nullable=false)
    */
   @Column({
-    name: "USP_FREQUENCY",
-    type="integer", nullable=false)
+    name: 'USP_FREQUENCY',
+    type: 'int',
+    nullable: false,
+    default: 30
   })
-  frequency;
+  frequency?: number;
 
   /**
    * @ORM\Column(name="USP_HMD", type="string", length=5, nullable=false)
    */
   @Column({
-    name: "USP_HMD",
-    type="string", length=5, nullable=false)
+    name: 'USP_HMD',
+    type: 'varchar',
+    length: 5,
+    nullable: false,
+    default: '08:00'
   })
-  hmd;
+  hmd?: string;
 
   /**
    * @ORM\Column(name="USP_HMF", type="string", length=5, nullable=false)
    */
   @Column({
-    name: "USP_HMF",
-    type="string", length=5, nullable=false)
+    name: 'USP_HMF',
+    type: 'varchar',
+    length: 5,
+    nullable: false,
+    default: '12:00'
   })
-  hmf;
+  hmf?: string;
 
   /**
    * @ORM\Column(name="USP_HAD", type="string", length=5, nullable=false)
    */
   @Column({
-    name: "USP_HAD",
-    type="string", length=5, nullable=false)
+    name: 'USP_HAD',
+    type: 'varchar',
+    length: 5,
+    nullable: false,
+    default: '14:00'
   })
-  had;
+  had?: string;
 
   /**
    * @ORM\Column(name="USP_HAF", type="string", length=5, nullable=false)
    */
   @Column({
-    name: "USP_HAF",
-    type="string", length=5, nullable=false)
+    name: 'USP_HAF',
+    type: 'varchar',
+    length: 5,
+    nullable: false,
+    default: '20:00'
   })
-  haf;
+  haf?: string;
 
   /**
    * @ORM\Column(name="USP_HEIGHT_LINE", type="integer", nullable=false)
    */
   @Column({
-    name: "USP_HEIGHT_LINE",
-    type="integer", nullable=false)
+    name: 'USP_HEIGHT_LINE',
+    type: 'int',
+    nullable: false,
+    default: 0
   })
-  heightLine;
+  heightLine?: number;
 
   /**
    * @ORM\Column(name="USP_QUOTATION_DISPLAY_ODONTOGRAM", type="string")
    * @var string 
    */
   @Column({
-    @ORM\Column(name="USP_QUOTATION_DISPLAY_ODONTOGRAM", type="string")
-ation: isplayO
-ontogram;
+    name: "USP_QUOTATION_DISPLAY_ODONTOGRAM",
+    type: 'enum',
+    enum: UserPreferenceQuotationDisplayOdontogramType,
+    nullable: false,
+    default: UserPreferenceQuotationDisplayOdontogramType.NONE
+  })
+  quotationDisplayOdontogram?: UserPreferenceQuotationDisplayOdontogramType;
 
   /**
    * @ORM\Column(name="USP_QUOTATION_DISPLAY_DETAILS", type="string")
    * @var string 
    */
   @Column({
-    @ORM\Column(name="USP_QUOTATION_DISPLAY_DETAILS", type="string")
-ation: isplayD
-tails;
+    name: 'USP_QUOTATION_DISPLAY_DETAILS',
+    type: 'enum',
+    enum: UserPreferenceQuotationDisplayDetailsType,
+    nullable: false,
+    default: UserPreferenceQuotationDisplayDetailsType.BOTH
+  })
+  quotationDisplayDetails?: UserPreferenceQuotationDisplayDetailsType;
 
   /**
    * @ORM\Column(name="USP_QUOTATION_DISPLAY_TOOLTIP", type="integer")
    * @var boolean 
    */
   @Column({
-    @ORM\Column(name="USP_QUOTATION_DISPLAY_TOOLTIP", type="integer")
-ation: isplayT
-oltip;
+    name: 'USP_QUOTATION_DISPLAY_TOOLTIP',
+    type: 'tinyint',
+    length: 1,
+    nullable: false,
+    default: 1
+  })
+  quotationDisplayTooltip?: number;
 
   /**
    * @ORM\Column(name="USP_QUOTATION_DISPLAY_DUPLICATA", type="integer")
    * @var boolean 
    */
   @Column({
-    @ORM\Column(name="USP_QUOTATION_DISPLAY_DUPLICATA", type="integer")
-ation: isplayD
-plicata;
+    name: 'USP_QUOTATION_DISPLAY_DUPLICATA',
+    type: 'tinyint',
+    length: 1,
+    nullable: false,
+    default: 0
+  })
+  quotationDisplayDuplicata?: number;
 
   /**
    * @ORM\Column(name="USP_QUOTATION_COLOR", type="string", nullable=true)
    * @var string 
    */
   @Column({
-    @ORM\Column(name="USP_QUOTATION_COLOR", type="string", nullable=true)
-ation: olor;
+    name: 'USP_QUOTATION_COLOR',
+    type: 'varchar',
+    nullable: true,
+  })
+  quotationColor?: string;
 
   /**
    * @ORM\Column(name="USP_BILL_DISPLAY_TOOLTIP", type="integer")
    * @var boolean
    */
   @Column({
-    @ORM\Column(name="USP_BILL_DISPLAY_TOOLTIP", type="integer")
-Display: ooltip;
+    name: 'USP_BILL_DISPLAY_TOOLTIP',
+    type: 'tinyint',
+    nullable: false,
+    default: 1
+  })
+  billDisplayTooltip?: 1;
 
   /**
    * @ORM\Column(name="USP_BILL_TEMPLATE", type="integer")
    * @var integer Modèle de facture utilisé.
    */
   @Column({
-    @ORM\Column(name="USP_BILL_TEMPLATE", type="integer")
-Template: 
+    name: 'USP_BILL_TEMPLATE',
+    type: 'tinyint',
+    length: 4,
+    nullable: false,
+    default: 1
+  })
+  billTemplate?: number;
 
   /**
    * @ORM\Column(name="USP_ORDER_DISPLAY_TOOLTIP", type="integer")
    * @var boolean
    */
   @Column({
-    @ORM\Column(name="USP_ORDER_DISPLAY_TOOLTIP", type="integer")
-r: isplayTooltip;
+    name: 'USP_ORDER_DISPLAY_TOOLTIP',
+    type: 'tinyint',
+    length: 1,
+    nullable: false,
+    default: 1
+  })
+  orderDisplayTooltip?: number;
 
   /**
    * @ORM\Column(name="USP_ORDER_DUPLICATA", type="integer")
    * @var boolean
    */
   @Column({
-    @ORM\Column(name="USP_ORDER_DUPLICATA", type="integer")
-r: uplicata;
+    name: 'USP_ORDER_DUPLICATA',
+    type: 'tinyint',
+    length: 1,
+    nullable: false,
+    default: 1
+  })
+  orderDuplicata?: number;
 
   /**
    * @ORM\Column(name="USP_ORDER_PREPRINTED_HEADER", type="integer")
    * @var boolean
    */
   @Column({
-    @ORM\Column(name="USP_ORDER_PREPRINTED_HEADER", type="integer")
-r: reprintedH
-ader;
+    name: 'USP_ORDER_PREPRINTED_HEADER',
+    type: 'tinyint',
+    length: 1,
+    nullable: false,
+    default: 0
+  })
+  orderPreprintedHeader?: number;
 
   /**
    * @ORM\Column(name="USP_ORDER_PREPRINTED_HEADER_SIZE", type="integer")
    * @var integer
    */
   @Column({
-    @ORM\Column(name="USP_ORDER_PREPRINTED_HEADER_SIZE", type="integer")
-r: reprintedH
-aderSize;
+    name: 'USP_ORDER_PREPRINTED_HEADER_SIZE',
+    type: 'int',
+    nullable: false,
+    default: 35
+  })
+  orderPreprintedHeaderSize?: number;
 
   /**
    * @ORM\Column(name="USP_ORDER_FORMAT", type="string", length=7)
    * @var string Format de l'ordonnance "A4", "A5" ou "widthxheight".
    */
   @Column({
-    @ORM\Column(name="USP_ORDER_FORMAT", type="string", length=7)
-r: ormat;
+    name: 'USP_ORDER_FORMAT',
+    type: 'varchar',
+    length: 7,
+    nullable: false,
+    default: 'A4'
+  })
+  orderFormat?: string;
 
   /**
    * @ORM\Column(name="USP_ORDER_BCB_CHECK", type="integer")
    * @var boolean Activation du contrôle depuis la Base Claude Bernard.
    */
   @Column({
-    @ORM\Column(name="USP_ORDER_BCB_CHECK", type="integer")
-r: cbC
-eck;
+    name: 'USP_ORDER_BCB_CHECK',
+    type: 'tinyint',
+    length: 1,
+    nullable: false,
+    default: 1
+  })
+  orderBcbCheck?: number;
 
   /**
    * @ORM\Column(name="USP_THEME_CUSTOM", type="integer")
    * @var integer
    */
   @Column({
-    @ORM\Column(name="USP_THEME_CUSTOM", type="integer")
-e: ustom;
+    name: 'USP_THEME_CUSTOM',
+    type: 'tinyint',
+    length: 4,
+    nullable: false,
+    default: 0
+  })
+  themeCustom?: number;
 
   /**
    * @ORM\Column(name="USP_THEME_COLOR", type="integer", nullable=true)
    * @var integer
    */
   @Column({
-    @ORM\Column(name="USP_THEME_COLOR", type="integer", nullable=true)
-e: olor;
+    name: 'USP_THEME_COLOR',
+    type: 'int',
+    nullable: true,
+  })
+  themeColor?: number;
 
   /**
    * @ORM\Column(name="USP_THEME_BGCOLOR", type="integer", nullable=true)
    * @var integer
    */
   @Column({
-    @ORM\Column(name="USP_THEME_BGCOLOR", type="integer", nullable=true)
-e: gcolor;
+    name: 'USP_THEME_BGCOLOR',
+    type: 'int',
+    nullable: true,
+  })
+  themeBgcolor?: number;
 
   /**
    * @ORM\Column(name="USP_THEME_BORDERCOLOR", type="integer", nullable=true)
    * @var integer
    */
   @Column({
-    @ORM\Column(name="USP_THEME_BORDERCOLOR", type="integer", nullable=true)
-e: ordercolor;
+    name: 'USP_THEME_BORDERCOLOR',
+    type: 'int',
+    nullable: true
+  })
+  themeBordercolor?: number;
 
   /**
    * @ORM\Column(name="USP_THEME_ASIDE_BGCOLOR", type="integer", nullable=true)
    * @var integer
    */
   @Column({
-    @ORM\Column(name="USP_THEME_ASIDE_BGCOLOR", type="integer", nullable=true)
-e: sideB
-color;
+    name: 'USP_THEME_ASIDE_BGCOLOR',
+    type: 'int',
+    nullable: true
+  })
+  themeAsideBgcolor?: number;
 
   /**
    * @ORM\Column(name="USP_REMINDER_VISIT_DURATION", type="integer")
    * @var integer Nombre de mois pour les rappels visites
    */
   @Column({
-    @ORM\Column(name="USP_REMINDER_VISIT_DURATION", type="integer")
-nder: isitD
-ration;
+    name: 'USP_REMINDER_VISIT_DURATION',
+    type: 'int',
+    nullable: false,
+    default: 6
+  })
+  reminderVisitDuration?: number;
 
   /**
    * @ORM\Column(name="USP_CCAM_BRIDGE_QUICKENTRY", type="integer")
    * @var boolean Saisie rapide des bridges.
    */
   @Column({
-    @ORM\Column(name="USP_CCAM_BRIDGE_QUICKENTRY", type="integer")
-Bridge: uickentry;
+    name: 'USP_CCAM_BRIDGE_QUICKENTRY',
+    type: 'tinyint',
+    nullable: false,
+    default: 0
+  })
+  ccamBridgeQuickentry?: number;
 
   /**
    * @ORM\Column(name="ccam_price_list", type="integer")
    * @var integer Grille tarifaire utilisée en CCAM.
    */
   @Column({
-    @ORM\Column(name="ccam_price_list", type="integer")
-Price: ist;
+    name: 'ccam_price_list',
+    type: 'tinyint',
+    nullable: false,
+    default: 13
+  })
+  ccamPriceList?: number;
+
+  /**
+   * @ORM\Column(name="ccam_price_list", type="integer", options={"default": 13})
+   * @Serializer\Expose
+   * @Serializer\Groups({"detail"})
+   * @Serializer\Type("int")
+   * @Assert\Type("int")
+   * @Assert\NotNull
+   * @Assert\Range(min=0, max=16)
+   */
+  @Column({
+    name: 'ccam_price_list',
+    type: 'tinyint',
+    nullable: false,
+    default: 13
+  })
+  priceGrid?: number;
 
   /**
    * Temps de prise en charge du patient.
@@ -385,27 +545,53 @@ Price: ist;
    * @var \DateTime
    */
   @Column({
-    @ORM\Column(name="patient_care_time", type="time")
-ent: areT
-me;
+    name: 'patient_care_time',
+    type: 'time',
+    nullable: false,
+    default: '00:00:00'
+  })
+  patientCareTime?: string;
+
+  /**
+   * @ORM\Column(name="sesam_vitale_mode_desynchronise", type="boolean", options={"default": false})
+   * @Serializer\Expose
+   * @Serializer\Groups({"detail"})
+   * @Serializer\Type("bool")
+   * @Assert\Type("bool")
+   * @Assert\NotNull
+   */
+  @Column({
+    name: 'sesam_vitale_mode_desynchronise',
+    type: 'tinyint',
+    length: 1,
+    nullable: false,
+    default: 0
+  })
+  sesamVitaleModeDesynchronise?: number;
 
   /**
    * @ORM\Column(name="calendar_border_colored", type="boolean", options={"default": true})
    */
   @Column({
-    name: "calendar_border_colored",
-    type="boolean", options={"default": true})
+    name: 'calendar_border_colored',
+    type: 'tinyint',
+    length: 1,
+    nullable: false,
+    default: 1
   })
-  calendarBorderColored = true;
+  calendarBorderColored?: number;
 
   /**
    * @ORM\Column(name="signature_automatic", type="boolean", options={"default": false})
    */
   @Column({
-    name: "signature_automatic",
-    type="boolean", options={"default": false})
+    name: 'signature_automatic',
+    type: 'tinyint',
+    length: 1,
+    nullable: false,
+    default: 0
   })
-  signatureAutomatic = false;
+  signatureAutomatic?: number;
 
   /**
    * @ORM\OneToOne(targetEntity="\App\Entities\User", inversedBy="preference")
@@ -413,7 +599,17 @@ me;
    * @ORM\Id
    * @ORM\GeneratedValue(strategy="NONE")
    */
-  user;
+  /**
+   * Entité de l'utilisateur.
+   * 
+   * @ORM\OneToOne(targetEntity="UserEntity", inversedBy="setting")
+   * @ORM\JoinColumn(name="USR_ID", referencedColumnName="USR_ID")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="NONE")
+   * @var \App\Entities\UserEntity
+   */
+  // @TODO EntityMissing
+  // protected $user;
 }
 
 
