81db05599e7ac1122fe062860214db0d4fe75c68
tieNguyen <ntien@MacbookPros-MacBook-Pro.local>
2023-06-05 09:31:39 +0700

    convert entity unit.php

---
+++ src/entities/reminder-unit.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="T_REMINDER_UNIT_RMU")
+ */
+@Entity('T_REMINDER_UNIT_RMU')
+export class ReminderUnitEntity {
+
+  /**
+   * @ORM\Column(name="RMU_ID", type="integer", nullable=false)
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="IDENTITY")
+   */
+  @PrimaryGeneratedColumn('increment', {
+    name: 'RMU_ID',
+  })
+  id?: number;
+
+  /**
+   * @ORM\Column(name="RMU_NAME", type="string", length=45, nullable=false)
+   */
+  @Column({
+    name: 'RMR_NAME',
+    length: 45,
+  })
+  name?: string;
+
+  /**
+   * @ORM\Column(name="RMU_NBR", type="integer", nullable=false)
+   */
+  @Column({
+    name: 'RMU_NBR'
+  })
+  nbr?: number;
+
+}
