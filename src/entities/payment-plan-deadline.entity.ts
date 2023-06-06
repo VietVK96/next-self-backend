5fcc9a129c01bc816f1391d32ff5e43beb7814f8
congcuong095 <congcuong095@gmail.com>
2023-06-06 11:44:26 +0700

    - payment-plan-deadline.entity.ts - payment-plan.entity.ts - periodontal-chart.entity.ts - plan.entity.ts

---
+++ src/entities/payment-plan-deadline.entity.ts
@@ -0,0 +1,72 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PaymentPlanDeadlineRepository")
+ * @ORM\Table(name="payment_schedule_line")
+ * @Serializer\ExclusionPolicy("all")
+ */
+@Entity('payment_schedule_line')
+export class PaymentPlanDeadline {
+
+  /**
+   * @ORM\Id
+   * @ORM\GeneratedValue
+   * @ORM\Column(name="id", type="integer")
+   * @Serializer\Expose
+   * @Serializer\Groups({
+   *  "paymentPlan:read"
+   * })
+   * @Serializer\Type("int")
+   */
+  @PrimaryGeneratedColumn('increment', {
+    name: 'id',
+  })
+  id?: number;
+
+  /**
+   * @ORM\ManyToOne(targetEntity="PaymentPlan", inversedBy="deadlines")
+   * @ORM\JoinColumn(name="payment_schedule_id", referencedColumnName="id")
+   */
+  // @TODO EntityMissing
+  // protected $paymentPlan;
+
+  /**
+   * @ORM\Column(name="date", type="date")
+   * @Serializer\Expose
+   * @Serializer\Groups({
+   *  "paymentPlan:read"
+   * })
+   * @Serializer\Type("DateTime<'Y-m-d'>")
+   * @Assert\Date
+   * @Assert\NotNull
+   */
+  @Column({
+    name: 'date',
+    type: 'date',
+    nullable: false
+  })
+  dueDate?: string;
+
+  /**
+   * @ORM\Column(name="amount", type="decimal", precision=10, scale=2, options={"default": 0})
+   * @Serializer\Expose
+   * @Serializer\Groups({
+   *  "paymentPlan:read"
+   * })
+   * @Serializer\Type("float")
+   * @Assert\Type("float")
+   * @Assert\NotNull
+   * @Assert\GreaterThanOrEqual(0)
+   */
+  @Column({
+    name: 'amount',
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+    nullable: false
+  })
+  amount?: number;
+
+}
+
+//application/Entity/PaymentPlanDeadline.php
