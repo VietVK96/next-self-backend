05841aead82b1526357e1c8494d084ed996dc35c
hai13032001 <hai13032001@gmail.com>
2023-06-06 10:38:29 +0700

    convert entities ccamFamily.entity.ts

---
+++ src/entities/ccamFamily.entity.ts
@@ -0,0 +1,65 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CcamFamilyRepository")
+ * @ORM\Table(name="ccam_family",
+ *  uniqueConstraints={
+ *      @ORM\UniqueConstraint(name="UNIQ_592EC78D77153098", columns={"code"})
+ *  }
+ * )
+ * @Serializer\ExclusionPolicy("all")
+ */
+@Entity('ccam_family')
+export class CcamFamily {
+  /**
+   * @ORM\Id
+   * @ORM\GeneratedValue
+   * @ORM\Column(name="id", type="integer")
+   * @Serializer\Expose
+   * @Serializer\Groups({"list", "detail"})
+   * @Serializer\Type("int")
+   */
+  @PrimaryGeneratedColumn('increment', {
+    name: 'id',
+  })
+  id?: number;
+
+  /**
+   * @ORM\ManyToOne(targetEntity="CcamPanier")
+   * @ORM\JoinColumn(name="ccam_panier_id", referencedColumnName="id", nullable=true)
+   * @Serializer\Expose
+   * @Serializer\Groups({"list", "detail"})
+   */
+  // @TODO EntityMissing
+  //   protected $panier = null;
+
+  /**
+   * @ORM\Column(name="code", type="string", length=3, options={"fixed": true})
+   * @Serializer\Expose
+   * @Serializer\Groups({"list", "detail"})
+   * @Assert\Type("string")
+   * @Assert\NotBlank
+   * @Assert\Length(min=3, max=3)
+   */
+  @Column({
+    name: 'code',
+    type: 'char',
+    length: 3,
+  })
+  code?: string;
+
+  /**
+   * @ORM\Column(name="label", type="string", length=255)
+   * @Serializer\Expose
+   * @Serializer\Groups({"list", "detail"})
+   * @Assert\Type("string")
+   * @Assert\NotBlank
+   * @Assert\Length(max=255)
+   */
+  @Column({
+    name: 'code',
+    type: 'varchar',
+    length: 255,
+  })
+  label?: string;
+}
