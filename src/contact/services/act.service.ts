100f0b567f818feff16dd432ad02d9f29bbefa09
your rank? <trungnq@ltsgroup.tech>
2023-06-26 17:01:11 +0700

    API update traceability

--- src/contact/services/act.service.ts
+++ src/contact/services/act.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TraceabilityEntity } from 'src/entities/traceability.entity';
 import { Repository } from 'typeorm/repository/Repository';
+import { UpdateTraceabilitiesDto } from '../dto/act.contact.dto';
 
 @Injectable()
 export class ActServices {
@@ -15,5 +16,25 @@ export class ActServices {
       where: { actId: id },
     });
     return data;
+  }
+
+  async updateTraceabilities(id: number, payload: UpdateTraceabilitiesDto) {
+    const req = payload.traceabilities.map((up) => {
+      return up?.id
+        ? {
+            medicalDeviceId: up?.medicalDeviceId,
+            reference: up?.reference,
+            observation: up?.observation,
+            actId: id,
+            id: up?.id,
+          }
+        : {
+            medicalDeviceId: up?.medicalDeviceId,
+            reference: up?.reference,
+            observation: up?.observation,
+            actId: id,
+          };
+    });
+    return await this.traceabilityRepository.save(req);
   }
 }
