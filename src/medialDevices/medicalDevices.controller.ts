219bb839f4c0e48fbba33ab4449a668663f058ca
sinhPC <phancongsinha3@gmail.com>
2023-06-26 09:19:42 +0700

    medical device

---
+++ src/medialDevices/medicalDevices.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { FindMedicalDevicesService } from './services/find.medicalDevices.service';
+import {
+  CurrentUser,
+  TokenGuard,
+  UserIdentity,
+} from 'src/common/decorator/auth.decorator';
+
+@ApiBearerAuth()
+@Controller('/medical-devices')
+@ApiTags('Medical devices')
+export class FindMedicalDevicesController {
+  constructor(private service: FindMedicalDevicesService) {}
+
+  // File php\contact\findAll.php 1->8
+  @Get()
+  @ApiQuery({
+    name: 'organization_id',
+  })
+  @UseGuards(TokenGuard)
+  async findAll(
+    @Query() { organization_id },
+    @CurrentUser() identity: UserIdentity,
+  ) {
+    return this.service.getMedicalDevices(organization_id);
+  }
+}
