5f5ffb043c4e44bca5e1dcc64f45103e48665d5c
sinhPC <phancongsinha3@gmail.com>
2023-06-27 09:05:40 +0700

    change response data

--- src/contact/services/act.service.ts
+++ src/contact/services/act.service.ts
@@ -13,10 +13,17 @@ export class ActServices {
   async getTraceability(id: number) {
     const data = await this.traceabilityRepository.find({
       where: { actId: id },
+      relations: {
+        medicalDevice: true,
+      },
     });
     return data.map((data) => {
       return {
         id: data.id,
+        medicalDevice: {
+          id: data.medicalDevice.id,
+          name: data.medicalDevice.name,
+        },
         observation: data.observation,
         reference: data.reference,
       };
