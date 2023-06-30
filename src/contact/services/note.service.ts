da1c4a5f10c74558c1662d3b60bcc67caa40c5b9
sinhPC <phancongsinha3@gmail.com>
2023-06-30 14:59:05 +0700

    change request user id

--- src/contact/services/note.service.ts
+++ src/contact/services/note.service.ts
@@ -9,6 +9,7 @@ import { SuccessResponse } from 'src/common/response/success.res';
 import { StoreNoteDto } from '../dto/noteStore.dto';
 import { UserService } from 'src/user/services/user.service';
 import { PatientService } from 'src/patient/service/patient.service';
+import { UserIdentity } from 'src/common/decorator/auth.decorator';
 
 @Injectable()
 export class NoteService {
@@ -18,9 +19,10 @@ export class NoteService {
     private userService: UserService,
     private patientService: PatientService,
   ) {}
-  async store(payload: StoreNoteDto) {
+  async store(payload: StoreNoteDto, identity: UserIdentity) {
     try {
-      const note = await this.repo.save(payload);
+      const newNote = { ...payload, userId: identity.id };
+      const note = await this.repo.save(newNote);
 
       // TODO add LOG
       //Ids\Log::write('Commentaire', $patientId, 1);
