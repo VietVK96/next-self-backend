f193db8122165d193814fa24b0bba178782140d6
congcuong095 <congcuong095@gmail.com>
2023-06-30 13:30:04 +0700

    api find one qoute

--- src/plans/services/plan.service.ts
+++ src/plans/services/plan.service.ts
@@ -4,10 +4,17 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { DataSource } from 'typeorm';
-import { DeleteOneStructDto, FindAllStructDto } from '../dto/plan.dto';
+import { FindAllStructDto, IdStructDto } from '../dto/plan.dto';
 import { UserIdentity } from 'src/common/decorator/auth.decorator';
 import { PermissionService } from 'src/users/services/permission.service';
 import { PaymentPlanService } from 'src/payment-plan/services/payment-plan.service';
+import {
+  EventData,
+  PlanEvent,
+  TaskData,
+  findOnePlanRes,
+} from '../response/plan.res';
+import { CBadRequestException } from 'src/common/exceptions/bad-request.exception';
 
 @Injectable()
 export class PlanService {
@@ -16,6 +23,263 @@ export class PlanService {
     private paymentPlanService: PaymentPlanService,
     private dataSource: DataSource,
   ) {}
+
+  async _getPlan(id: number, groupId: number): Promise<findOnePlanRes> {
+    let plan: findOnePlanRes = null;
+    const planQuery = `
+    SELECT
+        T_PLAN_PLF.PLF_ID AS id,
+        T_PLAN_PLF.user_id,
+        T_PLAN_PLF.patient_id,
+        T_PLAN_PLF.payment_schedule_id,
+        T_PLAN_PLF.PLF_NAME AS name,
+        T_PLAN_PLF.PLF_TYPE AS type,
+        T_PLAN_PLF.PLF_AMOUNT AS amount,
+        T_PLAN_PLF.PLF_MUTUAL_CEILING AS mutualCeiling,
+        T_PLAN_PLF.PLF_PERSON_REPAYMENT AS amount_repaid,
+        T_PLAN_PLF.PLF_PERSON_AMOUNT AS amount_to_be_paid,
+        IF (UNIX_TIMESTAMP(T_PLAN_PLF.PLF_ACCEPTED_ON) = 0, NULL, T_PLAN_PLF.PLF_ACCEPTED_ON) AS acceptedOn,
+        T_PLAN_PLF.BIL_ID AS bill_id,
+        T_DENTAL_QUOTATION_DQO.DQO_ID AS quote_id,
+        T_DENTAL_QUOTATION_DQO.DQO_TYPE AS quote_template
+    FROM T_PLAN_PLF
+    LEFT OUTER JOIN T_DENTAL_QUOTATION_DQO ON T_DENTAL_QUOTATION_DQO.PLF_ID = T_PLAN_PLF.PLF_ID 
+    WHERE T_PLAN_PLF.PLF_ID = ?
+    GROUP BY T_PLAN_PLF.PLF_ID`;
+    const plans: findOnePlanRes[] = await this.dataSource.query(planQuery, [
+      id,
+    ]);
+    plan = plans[0];
+    if (plan) {
+      plan.displayBill = false;
+      plan.bill = null;
+      plan.quotation = null;
+      if (plan.quote_id) {
+        const quoteId = plan.quote_id;
+        const quoteTemplate = plan.quote_template;
+
+        plan.quotation = {
+          id: quoteId,
+          template: quoteTemplate,
+          link: {
+            print: quoteId,
+            email: quoteId,
+          },
+        };
+      }
+
+      if (plan.bill_id) {
+        plan.bill = {
+          id: plan.bill_id,
+        };
+      }
+
+      plan.payment_schedule = null;
+      if (plan.payment_schedule_id) {
+        plan.payment_schedule = await this.paymentPlanService.find(
+          plan.payment_schedule_id,
+          groupId,
+        );
+      }
+
+      plan.bill_id = null;
+      plan.quote_id = null;
+      plan.quote_template = null;
+      plan.payment_schedule_id = null;
+
+      const eventStatement = `
+      SELECT
+        PLV.PLV_POS as plv_pos,
+        PLV.duration as plv_duration,
+        PLV.PLV_DELAY as plv_delay,
+        EVT.EVT_ID as id,
+        EVT.EVT_NAME as name,
+        EVT.EVT_COLOR as color,
+        IF (EVT.EVT_START IS NULL, NULL, CONCAT_WS(' ', evo.evo_date, TIME(EVT.EVT_START))) AS start,
+        IF (EVT.EVT_END IS NULL, NULL, CONCAT_WS(' ', evo.evo_date, TIME(EVT.EVT_END))) AS end,
+        USR.USR_ID as usr_id,
+        CONCAT_WS(' ', USR.USR_LASTNAME, USR.USR_FIRSTNAME) as usr_display_name,
+        event_type.id AS event_type_id,
+        event_type.label AS event_type_label
+      FROM T_PLAN_EVENT_PLV PLV
+      JOIN T_EVENT_EVT EVT
+      JOIN event_occurrence_evo evo
+      LEFT OUTER JOIN T_USER_USR USR ON USR.USR_ID = EVT.USR_ID
+      LEFT JOIN event_type ON event_type.id = EVT.event_type_id
+      WHERE PLV.PLF_ID = ?
+        AND PLV.EVT_ID = EVT.EVT_ID
+        AND EVT.EVT_DELETE = 0
+        AND evo.evt_id = EVT.EVT_ID
+      ORDER BY plv_pos`;
+      const event: EventData[] = await this.dataSource.query(eventStatement, [
+        plan.id,
+      ]);
+
+      if (event) {
+        for (const e of event) {
+          const push: PlanEvent = {
+            id: e.id,
+            name: e.name,
+            color: e.color,
+            start: e.start,
+            end: e.end,
+            tasks: [],
+            user:
+              e.usr_id === null
+                ? null
+                : {
+                    id: e.usr_id,
+                    displayName: e.usr_display_name,
+                  },
+            plan: {
+              pos: e.plv_pos,
+              duration: e.plv_duration,
+              delay: e.plv_delay,
+            },
+            event_type: e.event_type_id
+              ? null
+              : {
+                  id: e.event_type_id,
+                  label: e.event_type_label,
+                },
+          };
+
+          const actStatement = `
+              SELECT
+                ETK.ETK_ID as id,
+                ETK.library_act_id,
+                ETK.library_act_quantity_id,
+                ETK.parent_id,
+                ETK.ETK_NAME as name,
+                ETK.ETK_POS as pos,
+                ETK.ETK_DURATION as duration,
+                ETK.ETK_AMOUNT as amount,
+                ETK.ETK_COLOR as color,
+                ETK.ETK_QTY as qty,
+                ETK.ETK_STATE as state,
+                ETK.ccam_family,
+                DET.ETK_ID as dental_id,
+                DET.ccam_id,
+                DET.ngap_key_id,
+                DET.dental_material_id,
+                DET.DET_TOOTH as dental_teeth,
+                DET.DET_TYPE as dental_type,
+                DET.DET_COEF as dental_coef,
+                DET.DET_EXCEEDING as dental_exceeding,
+                DET.exceptional_refund,
+                DET.DET_CODE as dental_code,
+                DET.DET_COMP as dental_comp,
+                DET.DET_PURCHASE_PRICE as dental_purchase_price,
+                DET.DET_CCAM_CODE as ccamCode,
+                DET.DET_CCAM_OPPOSABLE as ccamOpposable,
+                DET.DET_CCAM_NPC as ccamNPC,
+                DET.DET_CCAM_NR as ccamNR,
+                DET.DET_CCAM_TELEM as ccamTelem,
+                DET.DET_CCAM_MODIFIER as ccamModifier,
+                DET.DET_SECU_AMOUNT as secuAmount,
+                DET.DET_SECU_REPAYMENT as secuRepayment,
+                DET.DET_MUTUAL_REPAYMENT_TYPE as mutualRepaymentType,
+                DET.DET_MUTUAL_REPAYMENT_RATE as mutualRepaymentRate,
+                DET.DET_MUTUAL_REPAYMENT as mutualRepayment,
+                DET.DET_MUTUAL_COMPLEMENT as mutualComplement,
+                DET.DET_PERSON_REPAYMENT as personRepayment,
+                DET.DET_PERSON_AMOUNT as personAmount,
+                ccam_panier.id AS ccam_panier_id,
+                ccam_panier.code AS ccam_panier_code,
+                ccam_panier.label AS ccam_panier_label,
+                ccam_panier.color AS ccam_panier_color
+            FROM T_EVENT_TASK_ETK ETK
+            LEFT OUTER JOIN T_DENTAL_EVENT_TASK_DET DET ON DET.ETK_ID = ETK.ETK_ID
+            LEFT OUTER JOIN ccam ON ccam.id = DET.ccam_id
+            LEFT OUTER JOIN ccam_family ON ccam_family.id = ccam.ccam_family_id
+            LEFT OUTER JOIN ccam_panier ON ccam_panier.id = ccam_family.ccam_panier_id
+            WHERE ETK.EVT_ID = ?
+            GROUP BY ETK.ETK_ID
+            ORDER BY ETK.ETK_POS`;
+
+          const task: TaskData[] = await this.dataSource.query(actStatement, [
+            push.id,
+          ]);
+
+          if (task) {
+            for (const t of task) {
+              push.tasks.push({
+                id: t.id,
+                library_act_id: t.library_act_id,
+                library_act_quantity_id: t.library_act_quantity_id,
+                parent_id: t.parent_id,
+                name: t.name,
+                pos: t.pos,
+                duration: t.duration,
+                amount: t.amount,
+                color: t.color,
+                qty: t.qty,
+                state: t.state,
+                ccam_family: t.ccam_family,
+                dental:
+                  t.dental_id === null
+                    ? null
+                    : {
+                        teeth:
+                          t.dental_teeth === null
+                            ? []
+                            : t.dental_teeth.split(','),
+                        type: t.dental_type,
+                        coef: t.dental_coef,
+                        exceeding: t.dental_exceeding,
+                        exceptional_refund: t.exceptional_refund,
+                        code: t.dental_code,
+                        comp: t.dental_comp,
+                        purchasePrice: t.dental_purchase_price,
+                        ccam_id: t.ccam_id,
+                        ngap_key_id: t.ngap_key_id,
+                        dental_material_id: t.dental_material_id,
+
+                        ccamCode: t.ccamCode,
+                        ccamOpposable: t.ccamOpposable,
+                        ccamNPC: t.ccamNPC,
+                        ccamNR: t.ccamNR,
+                        ccamTelem: t.ccamTelem,
+                        ccamModifier: t.ccamModifier,
+
+                        secuAmount: t.secuAmount,
+                        secuRepayment: t.secuRepayment,
+                        mutualRepaymentType: t.mutualRepaymentType,
+                        mutualRepaymentRate: t.mutualRepaymentRate,
+                        mutualRepayment: t.mutualRepayment,
+                        mutualComplement: t.mutualComplement,
+                        personRepayment: t.personRepayment,
+                        personAmount: t.personAmount,
+                        ccam: !t.ccam_panier_id
+                          ? {}
+                          : {
+                              family: {
+                                panier: {
+                                  id: t.ccam_panier_id,
+                                  code: t.ccam_panier_code,
+                                  label: t.ccam_panier_label,
+                                  color: JSON.parse(t.ccam_panier_color),
+                                },
+                              },
+                            },
+                      },
+              });
+
+              if (t.state > 0) {
+                plan.displayBill = true;
+              }
+            }
+
+            plan.events = push;
+          }
+        }
+      }
+    } else {
+      throw new CBadRequestException('unknown plan');
+    }
+
+    return plan;
+  }
 
   /**
    * File: php\contact\plans\findAll.php, line 23->94
@@ -98,7 +362,7 @@ export class PlanService {
     return plans;
   }
 
-  async deleteOne(request: DeleteOneStructDto, identity: UserIdentity) {
+  async deleteOne(request: IdStructDto, identity: UserIdentity) {
     const { id } = request;
     const queryRunner = this.dataSource.createQueryRunner();
     const queryBuiler = this.dataSource.createQueryBuilder();
@@ -258,6 +522,28 @@ export class PlanService {
         await queryRunner.rollbackTransaction();
       }
 
+      return error;
+    }
+  }
+
+  async findOne(request: IdStructDto, organizationId: number) {
+    try {
+      const planificationId = request.id;
+      const planification = await this._getPlan(
+        planificationId,
+        organizationId,
+      );
+
+      //@TODO
+      // const planificationType = planification.type;
+      // if ($planificationType == 'plan') {
+      //   Ids\Log:: write('Plan de traitement', $planification['patient_id'], 0);
+      // } else if ($planificationType == 'quotation') {
+      //   Ids\Log:: write('Devis', $planification['patient_id'], 0);
+      // }
+      // return JSON.stringify(planification)
+      return planification;
+    } catch (error) {
       return error;
     }
   }
