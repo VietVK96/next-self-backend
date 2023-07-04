588cf13f53d2e4bfcf190b6521488fabb1aff2b0
hai13032001 <hai13032001@gmail.com>
2023-07-04 14:09:30 +0700

    add UseGuards

--- src/memo/memo.controller.ts
+++ src/memo/memo.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { MemoService } from './services/memo.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CreateUpdateMemoDto } from './dto/createUpdate.memo.dto';
 import { MemoRes } from './response/memo.res';
+import { TokenGuard } from 'src/common/decorator/auth.decorator';
 
 @Controller('memo')
 @ApiBearerAuth()
@@ -12,24 +21,28 @@ export class MemoController {
 
   //ecoodentist-1.31.0\php\memo\store.php
   @Post('save')
+  @UseGuards(TokenGuard)
   async create(@Body() payload: CreateUpdateMemoDto) {
     return this.memoService.create(payload);
   }
 
   //ecoodentist-1.31.0\php\memo\show.php
   @Get('show/:id')
+  @UseGuards(TokenGuard)
   async show(@Param('id') id: number) {
     return this.memoService.show(id);
   }
 
   //ecoodentist-1.31.0\php\memo\delete.php
   @Delete('delete/:id')
+  @UseGuards(TokenGuard)
   async delete(@Param('id') id: number) {
     return this.memoService.delete(id);
   }
 
   //ecoodentist-1.31.0\php\memo\update.php
   @Post('update/:id')
+  @UseGuards(TokenGuard)
   async update(@Param('id') id: number, @Body() payload: MemoRes) {
     return this.memoService.update(id, payload);
   }
