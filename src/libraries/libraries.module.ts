47109c1922b8f1cf1d1826c990a684565ce3043e
Your Name <you@example.com>
2023-07-04 20:15:54 +0700

    :tada: Add api libraries for patient

--- src/libraries/libraries.module.ts
+++ src/libraries/libraries.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LibraryActFamilyEntity } from 'src/entities/library-act-family.entity';
+import { LibraryActEntity } from 'src/entities/library-act.entity';
 import { LibrariesController } from './libraries.controller';
 import { LibrariesService } from './services/libraries.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LibraryActFamilyEntity])],
+  imports: [
+    TypeOrmModule.forFeature([LibraryActFamilyEntity, LibraryActEntity]),
+  ],
   controllers: [LibrariesController],
   providers: [LibrariesService, LibrariesController],
 })
--- src/libraries/services/libraries.service.ts
+++ src/libraries/services/libraries.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserIdentity } from 'src/common/decorator/auth.decorator';
 import { LibraryActFamilyEntity } from 'src/entities/library-act-family.entity';
+import { LibraryActEntity } from 'src/entities/library-act.entity';
 import { FindOptionsWhere, Repository } from 'typeorm';
 import { ActFamiliesDto } from '../dto/act-families.dto';
 
@@ -8,21 +10,56 @@ import { ActFamiliesDto } from '../dto/act-families.dto';
 export class LibrariesService {
   constructor(
     @InjectRepository(LibraryActFamilyEntity)
-    private libraryActRepo: Repository<LibraryActFamilyEntity>,
+    private libraryActFamilyRepo: Repository<LibraryActFamilyEntity>,
+    @InjectRepository(LibraryActEntity)
+    private libraryActRepo: Repository<LibraryActEntity>,
   ) {}
 
   /**
-   * php/libraries/act-families/index.php 10 -> 22
+   * php/libraries/act-families/index.php done
    *
    */
-  async getALl(request: ActFamiliesDto): Promise<LibraryActFamilyEntity[]> {
-    const where: FindOptionsWhere<LibraryActFamilyEntity> = {};
+  async getALl(
+    request: ActFamiliesDto,
+    identity: UserIdentity,
+  ): Promise<LibraryActFamilyEntity[]> {
+    const where: FindOptionsWhere<LibraryActFamilyEntity> = {
+      organizationId: identity.org,
+    };
+    if (request.used_only) {
+      where.used = 1;
+    }
+    const data = await this.libraryActFamilyRepo.find({
+      where,
+      order: {
+        position: 'ASC',
+        id: 'ASC',
+      },
+    });
+
+    return data;
+  }
+
+  /**
+   * php/libraries/act-families/acts/index.php 100%
+   */
+  async getAct(
+    id: number,
+    identity: UserIdentity,
+    request: ActFamiliesDto,
+  ): Promise<LibraryActEntity[]> {
+    const where: FindOptionsWhere<LibraryActEntity> = {
+      organizationId: identity.org,
+      libraryActFamilyId: id,
+    };
     if (request.used_only) {
       where.used = 1;
     }
     const data = await this.libraryActRepo.find({
+      where,
       order: {
-        position: 'DESC',
+        position: 'ASC',
+        id: 'ASC',
       },
     });
 
