5fd3b1f1c37bf7f3671445d503e59d643ab22c19
thanhnaxlotus <thanhna@lqa.com.vn>
2023-07-11 15:48:15 +0700

    ve-709, ve-710

---
+++ src/common/exceptions/request-exception.exception.ts
@@ -0,0 +1,18 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class RequestException extends HttpException {
+  constructor(
+    message: string,
+    objectOrError?: string | object | any,
+    description = 'Internal server error',
+  ) {
+    const data = {
+      msg: message,
+      data: objectOrError,
+    };
+    super(
+      HttpException.createBody(data, description, HttpStatus.BAD_REQUEST),
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
