76d39f8735cd92529075534d7f9f6c485e169430
Hai-anti-windows <hai13032001@gmail.com>
2023-07-13 09:16:02 +0700

    repair endpoint

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -16,6 +16,7 @@ export enum ErrorCode {
   PERMISSION_DENIED = 'PERMISSION_DENIED',
   INSERT_FAILED = 'INSERT_FAILED',
   ERROR_CARESHEET_ACTS_IS_EMPTY = 'error.caresheet.acts.is_empty',
+  MESSAGE_SHOULD_NOT_BE_BLANK = 'message should not be blank',
 }
 
 export enum HttpStatus {
