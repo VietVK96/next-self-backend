429fa170e1521c347dc89fd7916cf42bec76ccac
Nguyenpd-vincent <nguyenpd@ltsgroup.tech>
2023-07-18 15:16:39 +0700

    udpate

---
+++ src/dental/services/devisRequestAjax.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DentalQuotationEntity } from 'src/entities/dental-quotation.entity';
+import { Repository } from 'typeorm';
+import { DevisRequestAjaxDto } from '../dto/devisHN.dto';
+import dayjs from 'dayjs';
+import { DEFAULT_LOCALE } from 'src/constants/locale';
+import { id } from 'date-fns/locale';
+@Injectable()
+export class DevisHNServices {
+  constructor(
+    @InjectRepository(DentalQuotationEntity)
+    private dentalQuotationRepository: Repository<DentalQuotationEntity>,
+  ) {}
+
+  async requestAjax(payload: DevisRequestAjaxDto) {
+    const {
+      id_devisHN,
+      duration,
+      identPrat,
+      addrPrat,
+      identPat,
+      infosCompl,
+      couleur,
+      schemas,
+      amount,
+      personRepayment,
+      signaturePatient,
+      signaturePraticien,
+      prestations,
+    } = payload;
+    const name = payload.titreDevisHN;
+    const date = dayjs(payload.datedevisHN)
+      .locale(DEFAULT_LOCALE)
+      .format('d/m/Y');
+    const acceptedAt = dayjs(payload.date_acceptation)
+      .locale(DEFAULT_LOCALE)
+      .format('d/m/Y');
+    // const dentalQuotation = this.dentalQuotationRepository.find({
+    //   id: id_devisHN,
+    // });
+    return '';
+  }
+}
