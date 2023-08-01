d2ffbd136275c7548eaf65e54e3da9d3d753b35b
congcuong095 <congcuong095@gmail.com>
2023-08-01 19:29:08 +0700

    done

--- src/user-preference/services/user-preference.service.ts
+++ src/user-preference/services/user-preference.service.ts
@@ -9,6 +9,9 @@ import { UserPreferenceQuotationEntity } from 'src/entities/user-preference-quot
 import { UserEntity } from 'src/entities/user.entity';
 import { MobileSettingEntity } from 'src/entities/mobile-setting.entity';
 import { DomtomEntity } from 'src/entities/domtom.entities';
+import { UpdateUserPreferenceDto } from '../dto/user-preference.dto';
+import { UserMedicalEntity } from 'src/entities/user-medical.entity';
+import { SuccessResponse } from 'src/common/response/success.res';
 
 @Injectable()
 export class UserPreferenceService {
@@ -18,9 +21,23 @@ export class UserPreferenceService {
     private readonly dataSource: DataSource,
   ) {}
 
-  private _convertH2M(timeInHour) {
+  private _convertHourToMinute(timeInHour) {
     const timeParts = timeInHour.split(':');
     return Number(timeParts[0]) * 60 + Number(timeParts[1]);
+  }
+
+  private _convertMinuteToHour(MINUTES) {
+    const m = MINUTES % 60;
+    const h = (MINUTES - m) / 60;
+    const HHMM =
+      (h < 10 ? '0' : '') +
+      h.toString() +
+      ':' +
+      (m < 10 ? '0' : '') +
+      m.toString() +
+      ':' +
+      '00';
+    return HHMM;
   }
 
   async find(userId: number) {
@@ -37,7 +54,8 @@ export class UserPreferenceService {
 				T_USER_PREFERENCE_USP.USP_DISPLAY_LAST_PATIENTS AS display_recently_treated,
 				T_USER_PREFERENCE_USP.USP_REMINDER_VISIT_DURATION AS reminder_visit_duration,
 				T_USER_PREFERENCE_USP.ccam_price_list,
-				T_USER_PREFERENCE_USP.patient_care_time
+				T_USER_PREFERENCE_USP.patient_care_time,
+				T_USER_PREFERENCE_USP.sesam_vitale_mode_desynchronise
 			FROM T_USER_PREFERENCE_USP
 			WHERE T_USER_PREFERENCE_USP.USR_ID = ?`;
 
@@ -61,15 +79,12 @@ export class UserPreferenceService {
       where: { id: userId },
       relations: { medical: { domtom: true } },
     });
-    const setting = await this.userPreferenceRepo.findOneOrFail({
-      where: { usrId: userId },
-    });
     const mobileSetting = await this.dataSource
       .getRepository(MobileSettingEntity)
       .findOne({ where: { userId } });
     const domtom = await this.dataSource.getRepository(DomtomEntity).find();
 
-    preference.patient_care_time = this._convertH2M(
+    preference.patient_care_time = this._convertHourToMinute(
       preference.patient_care_time,
     );
     return {
@@ -80,11 +95,102 @@ export class UserPreferenceService {
         settings: user?.settings,
         medical: { domtom: user?.medical?.domtom },
       },
-      setting: {
-        sesamVitaleModeDesynchronise: setting?.sesamVitaleModeDesynchronise,
-      },
       mobileSetting: { sessionDuration: mobileSetting?.sessionDuration },
       domtomDepartments: domtom,
+    };
+  }
+
+  async update(
+    userId: number,
+    body: UpdateUserPreferenceDto,
+    organizationId: number,
+  ): Promise<SuccessResponse> {
+    const {
+      ccamPriceList,
+      displayLastPatients,
+      themeCustom,
+      themeColor,
+      themeBgcolor,
+      themeBordercolor,
+      themeAsideBgcolor,
+      reminderVisitDuration,
+      patient_care_time,
+      sesamVitaleModeDesynchronise,
+    } = body;
+
+    const patientCareTime = this._convertMinuteToHour(patient_care_time);
+
+    const userPreferenceEntity = await this.userPreferenceRepo.findOne({
+      where: { usrId: userId },
+    });
+    userPreferenceEntity.priceGrid = ccamPriceList;
+    userPreferenceEntity.displayLastPatients = displayLastPatients;
+    userPreferenceEntity.themeCustom = themeCustom;
+    userPreferenceEntity.reminderVisitDuration = reminderVisitDuration;
+    userPreferenceEntity.patientCareTime = patientCareTime;
+    userPreferenceEntity.themeColor = themeColor;
+    userPreferenceEntity.themeBgcolor = themeBgcolor;
+    userPreferenceEntity.themeBordercolor = themeBordercolor;
+    userPreferenceEntity.themeAsideBgcolor = themeAsideBgcolor;
+    userPreferenceEntity.sesamVitaleModeDesynchronise =
+      sesamVitaleModeDesynchronise;
+
+    await this.userPreferenceRepo.save(userPreferenceEntity);
+
+    let mobileSetting = await this.dataSource
+      .getRepository(MobileSettingEntity)
+      .findOne({ where: { userId } });
+
+    if (!mobileSetting) {
+      mobileSetting = new MobileSettingEntity();
+      mobileSetting.groupId = organizationId;
+      mobileSetting.userId = userId;
+    }
+
+    mobileSetting.sessionDuration = body?.mobileSetting?.sessionDuration;
+    await this.dataSource
+      .getRepository(MobileSettingEntity)
+      .save(mobileSetting);
+
+    const quote = await this.dataSource
+      .getRepository(UserPreferenceQuotationEntity)
+      .findOne({ where: { usrId: userId } });
+    quote.periodOfValidity = body?.quote?.period_of_validity;
+    await this.dataSource
+      .getRepository(UserPreferenceQuotationEntity)
+      .save(quote);
+
+    const amo = await this.dataSource
+      .getRepository(UserAmoEntity)
+      .findOne({ where: { userId } });
+    if (amo) {
+      amo.isTp = body?.amo?.is_tp;
+      await this.dataSource.getRepository(UserAmoEntity).save(amo);
+    }
+
+    const user = await this.dataSource.getRepository(UserEntity).findOneOrFail({
+      where: { id: userId },
+      relations: { medical: { domtom: true } },
+    });
+    const medical = user?.medical;
+    if (medical) {
+      medical.domtomId = body?.domtom_id;
+    }
+
+    await this.dataSource.getRepository(UserMedicalEntity).save(medical);
+    user.settings = {
+      ...user?.settings,
+      activateSendingAppointmentReminders:
+        !!body?.activateSendingAppointmentReminders,
+      displayAllWaitingRooms: !!body?.displayAllWaitingRooms,
+      printAdditionalPatientInformation:
+        !!body?.printAdditionalPatientInformation,
+    };
+
+    await this.dataSource.getRepository(UserEntity).save(user);
+
+    return {
+      success: true,
     };
   }
 }
--- src/user-preference/user-preference.controller.ts
+++ src/user-preference/user-preference.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Put, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 import { UserPreferenceService } from './services/user-preference.service';
@@ -7,6 +7,7 @@ import {
   CurrentUser,
   UserIdentity,
 } from 'src/common/decorator/auth.decorator';
+import { UpdateUserPreferenceDto } from './dto/user-preference.dto';
 
 @ApiTags('UserPreference')
 @Controller('user-preference')
@@ -20,5 +21,18 @@ export class UserPreferenceController {
   @UseGuards(TokenGuard)
   async getByUser(@CurrentUser() identity: UserIdentity) {
     return await this.userPreferenceService.getByUser(identity.id);
+  }
+
+  @Put()
+  @UseGuards(TokenGuard)
+  async update(
+    @CurrentUser() identity: UserIdentity,
+    @Body() body: UpdateUserPreferenceDto,
+  ) {
+    return await this.userPreferenceService.update(
+      identity.id,
+      body,
+      identity.org,
+    );
   }
 }
