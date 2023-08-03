8c9fe3a36dc9c0815f549cc992d86b94202bf554
nvh1307 <vn137288@gmail.com>
2023-08-03 12:10:34 +0700

    update response

--- src/settings/services/notification.service.ts
+++ src/settings/services/notification.service.ts
@@ -12,7 +12,22 @@ export class NotificationService {
         WHERE sendingLog.USR_ID = ?
         ORDER BY sendingLog.USH_USED DESC
         `;
-    const sendingLog = this.dataSource.query(query, [userID]);
-    return sendingLog;
+    const sendingLog = await this.dataSource.query(query, [userID]);
+    const res = [];
+
+    for (let i = 0; i < sendingLog.length; i++) {
+      const resItem = {
+        id: sendingLog.length > 0 && sendingLog[i].USH_ID,
+        userId: sendingLog.length > 0 && sendingLog[i].USR_ID,
+        sendingDate: sendingLog.length > 0 && sendingLog[i].USH_USED,
+        message: sendingLog.length > 0 && sendingLog[i].USH_MSG,
+        externalReferenceId: sendingLog.length > 0 && sendingLog[i].USH_OVH_ID,
+        createdAt: sendingLog.length > 0 && sendingLog[i].created_at,
+        updatedAt: sendingLog.length > 0 && sendingLog[i].updated_at,
+      };
+      res.push(resItem);
+    }
+
+    return res;
   }
 }
