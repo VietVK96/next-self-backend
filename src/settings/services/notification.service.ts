f0d73d699ceb80e7116c2af516bdc0ec39063402
nvh1307 <vn137288@gmail.com>
2023-08-03 14:15:31 +0700

    update response to fetch in UI

--- src/settings/services/notification.service.ts
+++ src/settings/services/notification.service.ts
@@ -17,13 +17,14 @@ export class NotificationService {
 
     for (let i = 0; i < sendingLog.length; i++) {
       const resItem = {
-        id: sendingLog.length > 0 && sendingLog[i].USH_ID,
-        userId: sendingLog.length > 0 && sendingLog[i].USR_ID,
-        sendingDate: sendingLog.length > 0 && sendingLog[i].USH_USED,
-        message: sendingLog.length > 0 && sendingLog[i].USH_MSG,
-        externalReferenceId: sendingLog.length > 0 && sendingLog[i].USH_OVH_ID,
-        createdAt: sendingLog.length > 0 && sendingLog[i].created_at,
-        updatedAt: sendingLog.length > 0 && sendingLog[i].updated_at,
+        id: sendingLog[i].USH_ID,
+        usrId: sendingLog[i].USR_ID,
+        sendingDate: sendingLog[i].USH_USED,
+        receiver: sendingLog[i].USH_RECEIVER,
+        message: sendingLog[i].USH_MSG,
+        externalReferenceId: sendingLog[i].USH_OVH_ID,
+        createdAt: sendingLog[i].created_at,
+        updatedAt: sendingLog[i].updated_at,
       };
       res.push(resItem);
     }
