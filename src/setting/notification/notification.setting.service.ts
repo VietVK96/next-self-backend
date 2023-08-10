7796a808478247865f263b261c4e72d6c4a7d30a
congcuong095 <congcuong095@gmail.com>
2023-08-10 16:36:31 +0700

    done

--- src/setting/notification/notification.setting.service.ts
+++ src/setting/notification/notification.setting.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import {
-  AddressEntity,
-  hasAllFieldsRequiredForBilling,
-} from 'src/entities/address.entity';
+import { AddressEntity } from 'src/entities/address.entity';
 import { UserEntity } from 'src/entities/user.entity';
 import { DataSource } from 'typeorm';
 import { OrderContextBilling } from './monetico/orderContextBilling';
@@ -19,117 +16,29 @@ import { SaveMessageNotificationDto } from './dto/saveMessage.notification.dto';
 import { ReminderTypeEntity } from 'src/entities/reminder-type.entity';
 import { CBadRequestException } from 'src/common/exceptions/bad-request.exception';
 import { ErrorCode } from 'src/constants/error';
+import { OrganizationEntity } from 'src/entities/organization.entity';
+import { SaveSmsShareDto } from './dto/notification.dto';
+import { SuccessResponse } from 'src/common/response/success.res';
 
 @Injectable()
 export class NotificationSettingService {
   constructor(private dataSource: DataSource) {}
 
-  async find(userId: number, request: Request): Promise<FindNotificationRes> {
-    const user = await this.dataSource.getRepository(UserEntity).findOneOrFail({
-      where: { id: userId },
-      relations: {
-        address: true,
-        group: {
-          address: true,
-        },
-      },
-    });
-    const products = {};
-    let address: AddressEntity = user?.address;
-    if (!hasAllFieldsRequiredForBilling(address)) {
-      address = user?.group?.address;
-    }
+  //application/Entities/Address.php
+  //application/Entities/AddressEntity.php
 
-    if (address && hasAllFieldsRequiredForBilling(address)) {
-      const billing = new OrderContextBilling(
-        address.street,
-        address.city,
-        address.zipCode,
-        address.countryAbbr,
-      );
-      billing.setFirstName(user.firstname);
-      billing.setLastName(user.lastname);
-      billing.setEmail(user.email);
+  _hasAllFieldsRequiredForBilling(address: AddressEntity): boolean {
+    return (
+      !!address?.street &&
+      !!address?.city &&
+      !!address?.zipCode &&
+      !!address?.countryAbbr
+    );
+  }
 
-      const client = new OrderContextClient();
-      client.setFirstName(user.firstname);
-      client.setLastName(user.lastname);
-      client.setEmail(user.email);
-
-      const context = new OrderContext(billing);
-      context.setOrderContextClient(client);
-      const reference = StringHelper.random('alnum', 12).toUpperCase();
-
-      const pack100smsPaymentRequest = new PaymentRequest(
-        reference,
-        19.0,
-        'EUR',
-        'FR',
-        context,
-      );
-      pack100smsPaymentRequest.setMail(user.email);
-      pack100smsPaymentRequest.setUrlRetourOk(request.url);
-      pack100smsPaymentRequest.setUrlRetourErreur(request.url);
-      pack100smsPaymentRequest.setTexteLibre(
-        JSON.stringify({
-          id: user.id,
-          lastName: user.lastname,
-          firstName: user.faxNumber,
-          product: 'PACK100SMS',
-        }),
-      );
-
-      const pack500smsPaymentRequest = new PaymentRequest(
-        reference,
-        69.0,
-        'EUR',
-        'FR',
-        context,
-        user.email,
-        request.url,
-        request.url,
-      );
-      pack500smsPaymentRequest.setTexteLibre(
-        JSON.stringify({
-          id: user.id,
-          lastName: user.lastname,
-          firstName: user.faxNumber,
-          product: 'PACK500SMS',
-        }),
-      );
-
-      const pack1000smsPaymentRequest = new PaymentRequest(
-        reference,
-        120.0,
-        'EUR',
-        'FR',
-        context,
-        user.email,
-        request.url,
-        request.url,
-      );
-      pack1000smsPaymentRequest.setTexteLibre(
-        JSON.stringify({
-          id: user.id,
-          lastName: user.lastname,
-          firstName: user.faxNumber,
-          product: 'PACK1000SMS',
-        }),
-      );
-
-      const monetico = new Monetico();
-      products['pack100sms'] = monetico.getFormFields(pack100smsPaymentRequest);
-      products['pack500sms'] = monetico.getFormFields(pack500smsPaymentRequest);
-      products['pack1000sms'] = monetico.getFormFields(
-        pack1000smsPaymentRequest,
-      );
-
-      if (user) {
-        delete user.group;
-        delete user.group;
-      }
-      const smsCount: { sumSms: number }[] = await this.dataSource.query(
-        `
+  async getSmsQuantity(userId: number) {
+    const smsCount: { sumSms: number }[] = await this.dataSource.query(
+      `
       SELECT SUM(
           CASE WHEN T_GROUP_GRP.GRP_SHARE_SMS = 1
           THEN T_USER_SMS_USS.USS_STOCK
@@ -144,15 +53,139 @@ export class NotificationSettingService {
       FROM T_USER_USR user
       JOIN T_GROUP_GRP on user.organization_id = T_GROUP_GRP.GRP_ID
       JOIN T_USER_SMS_USS on user.USR_ID = T_USER_SMS_USS.USR_ID`,
-        [userId],
-      );
-      return {
-        user,
-        smsQuantity: smsCount[0].sumSms,
-        products,
-        address,
-      };
+      [userId],
+    );
+    return smsCount;
+  }
+
+  async find(userId: number, request: Request): Promise<FindNotificationRes> {
+    const url = request.headers.origin + request.url;
+    if (!userId) throw new CBadRequestException(ErrorCode.FORBIDDEN);
+    const user = await this.dataSource.getRepository(UserEntity).findOne({
+      where: { id: userId },
+      relations: {
+        address: true,
+        group: {
+          address: true,
+        },
+      },
+    });
+    if (!user) throw new CBadRequestException(ErrorCode.NOT_FOUND_USER);
+    const products = [];
+    const addressOfUser: AddressEntity = user?.address;
+    const addressOfOrganization: AddressEntity = user?.group?.address;
+    let address: AddressEntity;
+    if (addressOfUser && this._hasAllFieldsRequiredForBilling(addressOfUser)) {
+      address = addressOfUser;
+    } else if (
+      addressOfOrganization &&
+      this._hasAllFieldsRequiredForBilling(addressOfOrganization)
+    ) {
+      address = addressOfOrganization;
     }
+
+    const billing = new OrderContextBilling(
+      address?.street,
+      address?.city,
+      address?.zipCode,
+      address?.countryAbbr,
+    );
+    billing.setFirstName(user?.firstname);
+    billing.setLastName(user?.lastname);
+    billing.setEmail(user?.email);
+
+    const client = new OrderContextClient();
+    client.setFirstName(user?.firstname);
+    client.setLastName(user?.lastname);
+    client.setEmail(user?.email);
+
+    const context = new OrderContext(billing);
+    context.setOrderContextClient(client);
+    const reference = StringHelper.random('alnum', 12).toUpperCase();
+
+    const pack100smsPaymentRequest = new PaymentRequest(
+      reference,
+      19.0,
+      'EUR',
+      'FR',
+      context,
+    );
+    pack100smsPaymentRequest.setMail(user?.email);
+    pack100smsPaymentRequest.setUrlRetourOk(url);
+    pack100smsPaymentRequest.setUrlRetourErreur(url);
+    pack100smsPaymentRequest.setTexteLibre(
+      JSON.stringify({
+        id: user?.id,
+        lastName: user?.lastname,
+        firstName: user?.faxNumber,
+        product: 'PACK100SMS',
+      }),
+    );
+
+    const pack500smsPaymentRequest = new PaymentRequest(
+      reference,
+      69.0,
+      'EUR',
+      'FR',
+      context,
+      user?.email,
+      url,
+      url,
+    );
+    pack500smsPaymentRequest.setTexteLibre(
+      JSON.stringify({
+        id: user?.id,
+        lastName: user?.lastname,
+        firstName: user?.faxNumber,
+        product: 'PACK500SMS',
+      }),
+    );
+
+    const pack1000smsPaymentRequest = new PaymentRequest(
+      reference,
+      120.0,
+      'EUR',
+      'FR',
+      context,
+      user?.email,
+      url,
+      url,
+    );
+    pack1000smsPaymentRequest.setTexteLibre(
+      JSON.stringify({
+        id: user.id,
+        lastName: user.lastname,
+        firstName: user.faxNumber,
+        product: 'PACK1000SMS',
+      }),
+    );
+
+    const monetico = new Monetico();
+
+    products.push({
+      pack100sms: monetico.getFormFields(pack100smsPaymentRequest),
+    });
+    products.push({
+      pack500sms: monetico.getFormFields(pack500smsPaymentRequest),
+    });
+    products.push({
+      pack1000sms: monetico.getFormFields(pack1000smsPaymentRequest),
+    });
+
+    const smsCount: { sumSms: number }[] = await this.getSmsQuantity(userId);
+    const respone = {
+      user: {
+        admin: user?.admin,
+        group: { smsShared: user?.group?.smsSharing },
+      },
+      smsQuantity: smsCount[0].sumSms,
+      products: products,
+      address: {
+        allFieldsRequiredForBilling:
+          this._hasAllFieldsRequiredForBilling(address),
+      },
+    };
+    return respone;
   }
 
   async findMessage(userId: number): Promise<FindMessageNotificationRes> {
@@ -233,6 +266,28 @@ WHERE T_REMINDER_MESSAGE_RMM.USR_ID = ?
       return new CBadRequestException(ErrorCode.SAVE_FAILED);
     } finally {
       await queryRunner.release();
+    }
+  }
+
+  async saveSmsShare(
+    organizationId: number,
+    payload: SaveSmsShareDto,
+  ): Promise<SuccessResponse> {
+    try {
+      if (!organizationId) throw new CBadRequestException(ErrorCode.FORBIDDEN);
+      const organization = await this.dataSource
+        .getRepository(OrganizationEntity)
+        .findOne({ where: { id: organizationId } });
+      if (!organization) throw new CBadRequestException(ErrorCode.NOT_FOUND);
+      organization.smsSharing = payload?.smsShared;
+      await this.dataSource
+        .getRepository(OrganizationEntity)
+        .save(organization);
+      return {
+        success: true,
+      };
+    } catch (error) {
+      throw new CBadRequestException(error?.message);
     }
   }
 }
