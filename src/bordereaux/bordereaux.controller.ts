bad1490611b0083db09384a59c89fac7f6aa828f
Hai-anti-windows <hai13032001@gmail.com>
2023-08-14 13:43:07 +0700

    done

--- src/bordereaux/bordereaux.controller.ts
+++ src/bordereaux/bordereaux.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Res, UseGuards } from '@nestjs/common';
 import { BordereauxService } from './bordereaux.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { TokenGuard } from 'src/common/decorator/auth.decorator';
@@ -17,7 +17,30 @@ export class BordereauxController {
    */
   @Get('show/:id')
   @UseGuards(TokenGuard)
-  findOne(@Param('id') id: string) {
-    return this.bordereauxService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return await this.bordereauxService.findOne(+id);
+  }
+
+  /**
+   * File php/bordereaux/show.php 100%
+   *
+   * @param id
+   * @returns
+   */
+  @Get('print/:id')
+  @UseGuards(TokenGuard)
+  async printPdf(@Res() res, @Param('id') id: number) {
+    const buffer = await this.bordereauxService.printPdf(id);
+    res.set({
+      // pdf
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': `attachment; filename=print.pdf`,
+      'Content-Length': buffer.length,
+      // prevent cache
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      Pragma: 'no-cache',
+      Expires: 0,
+    });
+    res.end(buffer);
   }
 }
--- src/bordereaux/bordereaux.service.ts
+++ src/bordereaux/bordereaux.service.ts
@@ -1,5 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import 'dayjs/locale/fr';
+import * as dayjs from 'dayjs';
+import * as path from 'path';
+import { CBadRequestException } from 'src/common/exceptions/bad-request.exception';
+import {
+  HandlebarsHelpers,
+  PdfTemplateFile,
+  customCreatePdf,
+} from 'src/common/util/pdf';
+import { ErrorCode } from 'src/constants/error';
 import { SlipCheckEntity } from 'src/entities/slip-check.entity';
 import { Repository } from 'typeorm';
 
@@ -17,10 +27,72 @@ export class BordereauxService {
    * @returns
    */
   async findOne(id: number): Promise<SlipCheckEntity[]> {
-    const slipCheck = this.slipCheckRepository.find({
+    const slipCheck = await this.slipCheckRepository.find({
       relations: ['libraryBank', 'cashings'],
       where: { id: id },
     });
     return slipCheck;
+  }
+
+  /**
+   * File php/bordereaux/print.php
+   *
+   * @param id
+   * @returns
+   */
+  async printPdf(id: number) {
+    try {
+      const slipCheck = await this.slipCheckRepository.find({
+        relations: [
+          'libraryBank',
+          'cashings',
+          'libraryBank.user',
+          'libraryBank.group',
+          'libraryBank.address',
+        ],
+        where: { id: id },
+      });
+
+      const filePath = path?.join(
+        process.cwd(),
+        'templates/bordereaux',
+        'index.hbs',
+      );
+
+      const options = {
+        format: 'A4',
+        displayHeaderFooter: true,
+        footerTemplate: '<div></div>',
+        headerTemplate: '<div></div>',
+        margin: {
+          left: '5mm',
+          top: '5mm',
+          right: '5mm',
+          bottom: '5mm',
+        },
+      };
+
+      const data = {
+        slipCheck: slipCheck.length > 0 ? slipCheck[0] : {},
+      };
+
+      const files: PdfTemplateFile[] = [
+        {
+          data,
+          path: filePath,
+        },
+      ];
+
+      const helpers = {
+        dateFr: (date) => {
+          return dayjs(date).locale('fr').format('dddd D MMMM YYYY');
+        },
+        count: (arr) => arr.length,
+      };
+
+      return customCreatePdf({ files, options, helpers });
+    } catch {
+      throw new CBadRequestException(ErrorCode.STATUS_INTERNAL_SERVER_ERROR);
+    }
   }
 }
